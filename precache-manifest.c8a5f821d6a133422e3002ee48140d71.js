c9b746f31458e4adb2366c0e55657bef8a1eb809
Duncan Kioi <duncan@griffinkenya.com>
2020-04-13 19:10:21 +0300

    Updates

---
+++ precache-manifest.c8a5f821d6a133422e3002ee48140d71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90dd2feeafd5166a227f627956656d0f",
+    "url": "/buildforsdg/index.html"
+  },
+  {
+    "revision": "e8356ae72c5b42c5ec49",
+    "url": "/buildforsdg/static/css/main.265932ad.chunk.css"
+  },
+  {
+    "revision": "30f638c12dafea1bc001",
+    "url": "/buildforsdg/static/js/2.6e5cd52a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buildforsdg/static/js/2.6e5cd52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8356ae72c5b42c5ec49",
+    "url": "/buildforsdg/static/js/main.32c6edd9.chunk.js"
+  },
+  {
+    "revision": "c14579d29b51899917ad",
+    "url": "/buildforsdg/static/js/runtime-main.e924e663.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/buildforsdg/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/buildforsdg/precache-manifest.c8a5f821d6a133422e3002ee48140d71.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/buildforsdg/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
