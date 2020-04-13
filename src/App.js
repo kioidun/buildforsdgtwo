efcaa9d4b1df21dbb3416470430d4e6b26f57a2d
Duncan Kioi <duncan@griffinkenya.com>
2020-04-14 01:12:08 +0300

    removed section

--- src/App.js
+++ src/App.js
@@ -110,18 +110,9 @@ render(){
       };
   return (
     <div class ="container">
-        <nav class = "navbar">
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#form">Form</a></li>
-            </ul>
-        </nav>
-        <section id="home">
-            <h1>COVID-19 Health Simulator</h1>
-            <p>Build for SDG</p>
-        </section>
         <section id="formLoading">
-            <h1>Fill in the form</h1>
+        <h1>COVID-19 Health Simulator</h1>
+            <h2>Fill in the form</h2>
             <form class='formtwo' id="form" onSubmit={this.handleSubmit} >
             <div class = "form-control">
                 <label for="population">Population</label>
